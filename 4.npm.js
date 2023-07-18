48c0c4596ba9a63d9d9f62fb0869477eaf5cf0c7
jinu babu <jinutvm@gmail.com>
2023-07-18 16:43:25 +0530

    Les4 v1

---
+++ 4.npm.js
@@ -0,0 +1,27 @@
+// npm will be installed along with node 
+// check npm version npm --version or npm -v
+
+// Create package.json
+// npm init 
+// npm init -y 
+
+// To install a package 
+// npm i <package name>
+// remove a package
+// npm uninstall <package-name>
+
+// const http = require('http')
+
+// const server = http.createServer((req,res)=>{
+//     if(req.url === '/'){
+//         res.end('<h1>Hello world</h1>')
+//     } else if(req.url==='/about'){
+//         res.end('<h1>About world</h1>')
+//     }
+//     else {
+//         res.end('<h1>Not a valid URL3</h1>')
+//     }
+// })
+
+// server.listen(4007)
+
