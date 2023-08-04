37ee33a56674bffca3437cc03665166b81e0bda1
jinu babu <jinutvm@gmail.com>
2023-08-04 12:43:20 +0530

    Lesson 6 V2

--- 6.Store-API/controller/products.js
+++ 6.Store-API/controller/products.js
@@ -14,37 +14,87 @@ const Product= require('../model/products')
     // }
     
 const getAllProducts = async (req,res) => {
-    try {
-        console.log(req.query)
-        const {featured,name,company} = req.query
+        const {featured,name,company, sort:sortReq, fields, numericFilter } = req.query
         const queryObject = {}
         if (featured){
             queryObject.featured = featured === 'true' ? true : false
         }
-        // if (name) {
-        //     queryObject.name = name
-        // }
         if (name) {
-            searchName = `/^${name}/`
-            queryObject.name = `/^${name}/`
-            console.log(searchName, typeof searchName)
+            //   queryObject.name = name
+            searchName = '^' + name
+            queryObject.name = {$regex : searchName, $options: 'i'}
         }
         if (company) {
-            queryObject.company = company
+            searchName = '^' + company
+            // queryObject.company = company
+            queryObject.company = {$regex : searchName, $options: 'i'}
+        }
+        // Query object for numeric fields
+
+        if (numericFilter){
+            // input - rating>=4.51,price>100
+            // Output - {price:{'$gt':100},rating:{'$gte': 4.51}}
+
+            const operatorMap = {
+                '>'  : '$gt',
+                '>=' : '$gte',
+                '<'  : '$lt',
+                '<=' : '$lte',
+                '='  : '$eq'
+            }            
+
+            const reqularExpression = /\b(<|>|>=|=|<|<=)\b/g;
+
+            let filter = numericFilter.replace(reqularExpression,(match)=>`-${operatorMap[match]}-`)
+            console.log(filter)
+
+            const options = ['price','rating']
+
+            filter = filter.split(',').forEach(element=>{
+                const [field,operator,value] = element.split('-')
+                if (options.includes(field)){
+                    queryObject[field] = {[operator]:Number(value)}
+                }
+            })
+
+
+            console.log(queryObject)
+            // Output - {price:{'$gt':100},rating:{'$gte': 4.51}}
+
+
+
         }
 
-        console.log(queryObject)
 
-        // const product = await Product.find({featured: featured,name:name})
-        // const product = await Product.find({name: /^a/ })
-        const product = await Product.find({name: {$regex: searchName }})
-        // const product = await Product.find(queryObject)
 
-        res.status(200).json({totalProducts: product.length, product})
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({msg: 'Something unexpected happened'})
-    }
+        let result = Product.find(queryObject)
+        if (sortReq){
+            const sortList = sortReq.split(',').join(' ')
+            result = result.sort(sortList)
+        } else {
+            result = result.sort('createdAt')
+        }
+
+        if (fields){
+            const fieldList = fields.split(',').join(' ')
+            result = result.select(fieldList)
+        }
+        
+        //pagination 
+        const page = req.query.page || 1
+        const limit = req.query.limit || 100
+        const skip = (page-1) * limit 
+
+        result = result.skip(skip).limit(limit)
+
+        try {
+            // const product = await Product.find(queryObject)
+            const product = await result
+            res.status(200).json({totalProducts: product.length, product})       
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({msg: 'Something unexpected happened'})
+        }
 
 
 }
@@ -53,7 +103,8 @@ const getAllProductsStatic = async (req,res) => {
     try {
         // const product = await Product.find({})
         console.log(req.query)
-        const product = await Product.find(req.query)
+        let product = await Product.find({price:{'$gt':100},rating:{'$gte': 4.51}}).select('name price rating')
+        // product.sort(req.query.name)
         res.status(200).json({totalProducts: product.length, product})
     } catch (error) {
         console.log(error)
